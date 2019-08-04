8f4711311a6ad347af6fa73781b1f2eeb55d0750
Nurym Kenzh <nak290284@gmail.com>
2019-08-05 00:26:18 +0600

    Add project files.

---
+++ Enrypt/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Enrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypt = new System.Windows.Forms.TextBox();
+            this.textBoxDecrypt = new System.Windows.Forms.TextBox();
+            this.buttonDo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Word:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Encrypted:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Decrypted:";
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.Location = new System.Drawing.Point(95, 13);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(644, 20);
+            this.textBoxWord.TabIndex = 3;
+            // 
+            // textBoxEncrypt
+            // 
+            this.textBoxEncrypt.Location = new System.Drawing.Point(95, 48);
+            this.textBoxEncrypt.Name = "textBoxEncrypt";
+            this.textBoxEncrypt.Size = new System.Drawing.Size(644, 20);
+            this.textBoxEncrypt.TabIndex = 4;
+            // 
+            // textBoxDecrypt
+            // 
+            this.textBoxDecrypt.Location = new System.Drawing.Point(95, 85);
+            this.textBoxDecrypt.Name = "textBoxDecrypt";
+            this.textBoxDecrypt.Size = new System.Drawing.Size(644, 20);
+            this.textBoxDecrypt.TabIndex = 5;
+            // 
+            // buttonDo
+            // 
+            this.buttonDo.Location = new System.Drawing.Point(95, 111);
+            this.buttonDo.Name = "buttonDo";
+            this.buttonDo.Size = new System.Drawing.Size(75, 23);
+            this.buttonDo.TabIndex = 6;
+            this.buttonDo.Text = "Encrypt";
+            this.buttonDo.UseVisualStyleBackColor = true;
+            this.buttonDo.Click += new System.EventHandler(this.buttonDo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 261);
+            this.Controls.Add(this.buttonDo);
+            this.Controls.Add(this.textBoxDecrypt);
+            this.Controls.Add(this.textBoxEncrypt);
+            this.Controls.Add(this.textBoxWord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxWord;
+        private System.Windows.Forms.TextBox textBoxEncrypt;
+        private System.Windows.Forms.TextBox textBoxDecrypt;
+        private System.Windows.Forms.Button buttonDo;
+    }
+}
+
---
+++ Enrypt/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDo_Click(object sender, EventArgs e)
+        {
+            textBoxEncrypt.Text = Encrypt(textBoxWord.Text);
+            textBoxDecrypt.Text = Decrypt(textBoxEncrypt.Text);
+        }
+
+        public static String Encrypt(String encryptstr)
+        {
+            try
+            {
+                byte[] data = ASCIIEncoding.ASCII.GetBytes(encryptstr);
+                byte[] rgbKey = ASCIIEncoding.ASCII.GetBytes("vY7q-3Os");
+                byte[] rgbIV = ASCIIEncoding.ASCII.GetBytes("_1pCq7Yw");
+
+                MemoryStream memoryStream = new MemoryStream(1024);
+                DESCryptoServiceProvider desCryptoServiceProvider = new DESCryptoServiceProvider();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, desCryptoServiceProvider.CreateEncryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+
+                byte[] result = new byte[(int) memoryStream.Position];
+                memoryStream.Position = 0;
+                memoryStream.Read(result, 0, result.Length);
+                cryptoStream.Close();
+                cryptoStream.Dispose();
+                return Convert.ToBase64String(result);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public static String Decrypt(String decryptstr)
+        {
+            string decrypted = string.Empty;
+            try
+            {
+                byte[] data = System.Convert.FromBase64String(decryptstr);
+                byte[] rgbKey = System.Text.ASCIIEncoding.ASCII.GetBytes("vY7q-3Os");
+                byte[] rgbIV = System.Text.ASCIIEncoding.ASCII.GetBytes("_1pCq7Yw");
+
+                MemoryStream memoryStream = new MemoryStream(data.Length);
+                DESCryptoServiceProvider desCryptoServiceProvider = new DESCryptoServiceProvider();
+                ICryptoTransform x = desCryptoServiceProvider.CreateDecryptor(rgbKey, rgbIV);
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, x, CryptoStreamMode.Read);
+                memoryStream.Write(data, 0, data.Length);
+
+                memoryStream.Position = 0;
+                decrypted = new StreamReader(cryptoStream).ReadToEnd();
+                cryptoStream.Close();
+                memoryStream.Dispose();
+                return decrypted;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
